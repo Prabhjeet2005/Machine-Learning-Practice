9e1096f34435026c7f747c3aed601dbc82906dd3
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-03-17 23:56:01 +0530

    feat: Add Flask app with basic routes and views

---
+++ Section18_Flask/1_flask/1_app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+'''
+instance of Flask class, it is WSGI (Web Server Gateway Interface) application
+
+'''
+app = Flask(__name__) # __name__ is entry point
+
+@app.route("/")
+def home():
+  return "This is home Route."
+
+@app.route("/second_page")
+def second_page():
+  return "This is Second Page. Okay"
+
+if __name__ == "__main__":  # Execution Starts here
+  app.run(debug=True) # debug = True -----> Automatic Restart the server
+
+
